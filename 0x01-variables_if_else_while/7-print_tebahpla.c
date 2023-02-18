3b208cc32da61eae4fdf951d59ee638c9a741e77
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-02-17 21:01:18 -0800

    Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - Entry points
+ * Description: Prints lowercase alphabetsin reverse
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
