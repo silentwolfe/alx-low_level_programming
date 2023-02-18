322ddf6004697e107c1d855a093f237668dd41de
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-02-17 20:51:06 -0800

    Prints all single digits numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n')
+	return (0);
+}
