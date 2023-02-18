9a1951dd4235bfafcce26da176a0e5c68924688a
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-02-17 19:38:08 -0800

    From lower to upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * Prints alphabets both in lowercase then in uppercase
+ *
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= "z") {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= "z") {
+			putchar(d);
+			d++;
+		}
+	putchar("\n");
+	return (0);
