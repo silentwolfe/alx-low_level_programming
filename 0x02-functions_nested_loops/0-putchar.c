805a452f2b06b90165c8bda97d8c62b63ebec60f
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-02-21 07:53:40 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints _putchar with a new line to the standard output
+ *
+ * Return: 0 (success);
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a readme, i love ALx #Doinghardthings
