ead6b7ed779a5abeb97a03b84db467e88b328c00
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-02-23 12:51:32 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,16 @@
 /**
  * print_numbers - checks for checks for a digit (0 through 9).
  *
- * Return: Always 0.
+ * Return: void.
  */
 void print_numbers(void)
 {
- int c;
+	char c;
 
- for (c = 48; c < 58; c++)
- {
- _putchar(c);
- }
- _putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
 }
 
