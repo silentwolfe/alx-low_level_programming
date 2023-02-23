8e9bc39b5fce3ff36ca12605fff3a3e20efd7317
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-02-23 10:33:23 -0800

    The multiple integer

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
-/** mul - This function prints the multiple of two variables
+/**
+ * mul - This function prints the multiple of two variables
+ *
+ * @a: Prototype function
+ * @b: prototype function
  *
  * Return: The multiple of two variables
  */
 int mul(int a, int b)
 {
-	return a * b;
+	mul = a * b;
+	return (mul);
 }
