dce2d0db954141475845888c0254159cc3a9043f
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-02-23 19:36:46 -0800

    Print 0 to 9 and leave 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers - This function prints all the numbers from 
+ * print_most_numbers - This function prints all the numbers from
  * 0 to 9 except 2 an 4
  *
  * Return: void
