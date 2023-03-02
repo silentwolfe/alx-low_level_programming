dda301bce89daf23fdfd368b227cd82b373e2638
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-03-02 05:25:23 -0800

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_number - print numbers chars
  * @n: integer params
@@ -14,7 +14,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n1 = -n;
 	}
 
@@ -22,6 +22,5 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-		putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
-
--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-/**
- * main - A program that prints a[2] = 98, followed by a new line.
- * Return: (0);
- */
 
 int main(void)
 {
