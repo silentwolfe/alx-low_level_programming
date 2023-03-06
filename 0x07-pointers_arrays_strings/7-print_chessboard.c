1643c083fdbf4d04f4394ac9a9fdb1d3043fd2ec
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-03-06 09:05:18 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_chessboard - This function creates a chessboard
+ * @a: prototype argument
+ * Return: 0;
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
