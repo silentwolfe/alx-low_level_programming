90ca4d2c9e19d8aeb3063fb8e3d0fab7fe3ad0e9
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-03-06 04:56:06 -0800

    This code creates a function that allows the copyof different memory areas

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main"
+/**
+ * _memcpy - This function copies the memory area,
+ * with this both variables should have the same data
+ * @dest: This is a pointer to the destination memory
+ * area where the bytes will be copied to
+ * @src: This is a pointer to the source memory area
+ * where the bytes will be copied from
+ * @n: This is the specified number of bytes to be
+ * copied to the dest memory area
+ * Return: dest (success);
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
