492675aa272e209def7f6afb66a0a373e630af7f
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-03-06 16:59:33 -0800

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,16 +5,18 @@
  * print_diagsums - Entry point
  * @a: input
  * @size: input
- * Return: Always 0
+ * Return: 0 always;
  */
+
 void print_diagsums(int *a, int size)
 {
-	int i, n, sum1 = 0, sum2 = 0;
+	int sum1 = 0, sum2 = 0;
+	int i;
 
-	for (i = 0; i <= (size * size); i = i + size + 1)
-		sum1 = sum1 + a[i];
-
-	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
-		sum2 = sum2 + a[a];
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + (size - 1 - i));
+	}
 	printf("%d, %d\n", sum1, sum2);
 }
