425c1151abba569d47c2c70ddfc17d9bc331d5b4
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-03-08 06:35:59 -0800

    This function prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - function prints a string followed by a new line
+ * @s:local  variable
+ * Return: 0 (success);
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
