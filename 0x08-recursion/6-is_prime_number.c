1c8d48efc7b55628dffa2d30bfac54a88d7c2cae
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-03-08 13:30:51 -0800

    Primes are important

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,36 @@
 #include "main.h"
+#include <math.h>
+/**
+ * is_divisible - entry point
+ * @n: local variable
+ * @i: loacl variable
+ * Return: 0 success
+ */
+
+int is_divisible(int n, int i)
+{
+	if (i == 1)
+		return (0);
+
+	if (n % i == 0)
+		return (1);
+
+	return (is_divisible(n, i - 1));
+}
+
 /**
  * is_prime_number - entry point
  * @n: local variable
  * Return: 0 success
  */
+
 int is_prime_number(int n)
 {
-	if (n < 2)
-	{
+	if (n <= 1)
 		return (0);
-	}
 
-	for (int i = 2; i < n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	if (n == 2)
+		return (1);
+
+	return (!is_divisible(n, (int) sqrt(n)));
 }
