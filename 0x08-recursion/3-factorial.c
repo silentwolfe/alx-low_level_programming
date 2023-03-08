796e42eab62c10482d177d283b26d96721b094a5
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-03-08 09:32:45 -0800

    This program return the factorail of every number given

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * factorial - This program returns the factorial of a given number
+ * @n: local varaible
+ * Return: 0 (succes);
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		int result = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			result (*= i);
+		}
+	return (result);
+    }
+}
+
+
+
+
+
