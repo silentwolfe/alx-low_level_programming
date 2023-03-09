4537b6c96642e19f3ee394ec5152ac509ffdf37d
Osaro Eboizegbe <eboizegbeosaro1@gmail.com>
2023-03-08 20:28:13 -0800

    Alx: 6.inception. is it possible

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,36 +1,31 @@
 #include "main.h"
-#include <math.h>
-/**
- * is_divisible - entry point
- * @n: local variable
- * @i: loacl variable
- * Return: 0 success
- */
-
-int is_divisible(int n, int i)
-{
-	if (i == 1)
-		return (0);
-
-	if (n % i == 0)
-		return (1);
-
-	return (is_divisible(n, i - 1));
-}
+int actual_prime(int n, int i);
 
 /**
- * is_prime_number - entry point
- * @n: local variable
- * Return: 0 success
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is aprime number, 0 if not
  */
-
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
+	return (actual_prime(n, n - 1));
+}
 
-	if (n == 2)
+/**
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int actual_prime(int n, int i)
+{
+	if (i == 1)
 		return (1);
-
-	return (!is_divisible(n, (int) sqrt(n)));
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
 }
