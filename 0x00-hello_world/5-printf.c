4968911b613dbf554fa5d9e8ba7d216b83da83db
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-06-15 21:57:48 -0700

    This program is based on the C program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This prints out an output using printf function
+ * Return: return 0 (success) else unsuccessful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
