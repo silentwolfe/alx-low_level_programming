74a1074a11cd08120cee21ae528608d015643f97
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-06-15 21:10:03 -0700

    This program is based on the C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - Prints a text file using puts
+ * return - Returns 0(successful)
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
