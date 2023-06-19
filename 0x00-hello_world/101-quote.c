d54de6f9e85f8d0352876746806febadafff17e1
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-06-19 06:17:25 -0700

    This is ALx

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,69 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - This porgram prints a sentence as it is written
+ * main - This program prints a sentence as it should
  *
- * Return: return 1 not 0
+ * Return: return 1 as failure if 0 stop
  */
 
 int main(void)
 {
-	putchar('a');
-	putchar('n');
-	putchar('d');
-	putchar(' ');
-	putchar('t');
-	putchar('h');
-	putchar('a');
-	putchar('t');
-	putchar(' ');
-	putchar('p');
-	putchar('i');
-	putchar('e');
-	putchar('c');
-	putchar('e');
-	putchar(' ');
-	putchar('a');
-	putchar('r');
-	putchar('t');
-	putchar(' ');
-	putchar('i');
-	putchar('s');
-	putchar(' ');
-	putchar('u');
-	putchar('s');
-	putchar('e');
-	putchar('f');
-	putchar('u');
-	putchar('l');
-	putchar('"');
-	putchar(' ');
-	putchar('-');
-	putchar(' ');
-	putchar('D');
-	putchar('o');
-	putchar('r');
-	putchar('a');
-	putchar(' ');
-	putchar('K');
-	putchar('o');
-	putchar('r');
-	putchar('p');
-	putchar('a');
-	putchar('r');
-	putchar(',');
-	putchar(' ');
-	putchar('2');
-	putchar('0');
-	putchar('1');
-	putchar('5');
-	putchar('-');
-	putchar('1');
-	putchar('0');
-	putchar('-');
-	putchar('1');
-	putchar('9');
-	putchar('\n');
-
-	return (0);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
