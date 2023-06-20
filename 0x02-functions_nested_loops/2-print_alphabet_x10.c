e4326767fcbc6b1eb08af5901e00bd44ebb07656
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-06-20 10:16:00 -0700

    This is a program on functions and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  This prints the alphabets in small letters 10 times
+ * void: no return type
+ */
+
+void print_alphabet_x10(void)
+{
+	char i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+	
+	_putchar('\n');
+	}
+}
