bf35933c6368549814fc481938c969eb35e49800
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-06-21 13:09:38 -0700

    Printing the addition of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Adss two integers and returns result
+ * @i: prototype parameter
+ * @j: prototype parameter
+ * Return: 0 always
+ */
+
+int add(int i, int j)
+{
+	int k = i + j;
+
+	return (k);
+}
+
