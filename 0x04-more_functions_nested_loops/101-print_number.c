44afb600fcda2075d1562f8c5c3053621944fe9d
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-06-22 22:12:51 -0700

    Functions and nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer to print
+ */
+void print_number(int n)
+{
+	int digit, divisor = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while (n / divisor > 9)
+	divisor *= 10;
+
+	while (divisor != 0)
+	{
+	 digit = n / divisor;
+	_putchar(digit + '0');
+	n %= divisor;
+	divisor /= 10;
+	}
+}
