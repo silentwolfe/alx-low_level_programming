6e5c474e5e100d2502f7fbc64f163e97a72bd4d4
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-06-23 18:56:56 -0700

    This is ALx

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,22 +6,16 @@
  */
 void print_number(int n)
 {
-	int digit, divisor = 1;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	while (n / divisor > 9)
-	divisor *= 10;
-
-	while (divisor != 0)
+	if (n / 10)
 	{
-	 digit = n / divisor;
-	_putchar(digit + '0');
-	n %= divisor;
-	divisor /= 10;
+		print_number(n / 10);
 	}
+
+	_putchar((n % 10) + '0');
 }
