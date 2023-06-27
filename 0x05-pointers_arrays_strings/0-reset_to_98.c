16e71bb18a4a678fdbfca77a1c7de32ed2919417
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-06-27 05:54:01 -0700

    This is a basic program on pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This program resets an integers value to 98
+ * *n: prototype declaration in the parameter
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This readme file is about pointers, array and strings
