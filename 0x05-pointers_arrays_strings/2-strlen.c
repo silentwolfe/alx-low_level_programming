7171f8264594b939f6a28194b00825f7b6c8bd87
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-06-27 08:33:38 -0700

    This is a program on pointers, arrays and strings and this program returns the length of a character

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - This program returns the length of a string
+ * @s: prototype declaration
+ *
+ * Return: integer;
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\n')
+	{
+		i++;
+	}
+	return (i);
+}
