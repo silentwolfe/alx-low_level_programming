7036bc117679cf23033374dd68cb90167cb2a058
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-06-27 23:45:01 -0700

    This is a program on pointers and arrays

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * puts2 - This function prints every other character of
@@ -9,15 +10,16 @@
 
 void puts2(char *str)
 {
-	if (str == NULL)
-		return (0);
-
 	int i = 0;
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
+
