29767ba56db380f80b3b5b300abaa21f9d8c1cbd
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-06-29 10:26:36 -0700

    Adds a function that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - This function concatenates a string
+ * @s1: Represents the first string.
+ * @s2: Represents the second string
+ *
+ * Return: unsigned chars.
+ */
+
+int _strcmp(const char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
