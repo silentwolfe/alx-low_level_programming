79dada01800da5232f19a30eb155a60c75c4b6c4
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-06-29 12:12:57 -0700

    adds a function that encodes a function

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet- This function capitalizes all strings
+ * @str: Represents the first string.
+ *
+ * Return: char.
+ */
+
+char *leet(char *str)
+{
+	char *ptr = str;
+	char *leetChars = "AEOTL";
+	char *leetReplacements = "43071";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; leetChars[j] != '\0'; j++)
+		{
+			if (str[i] == leetChars[j] || str[i] == leetChars[j] + 32)
+			{
+				str[i] = leetReplacements[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
