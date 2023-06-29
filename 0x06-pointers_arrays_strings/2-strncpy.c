25e1060b09693469e97a5d05b2d29b7d727e86b1
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-06-29 10:11:28 -0700

    Adds a function that copies strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,7 @@
  * the pointer to the destination string.
  */
 
-char *_strncpy(char *dest, const char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	char *original_dest = dest;
 
