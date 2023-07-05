b51cd2200155cf0cd51ba62258d713a0d08e58d5
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-07-05 14:19:47 -0700

    Initial commit for the reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - This function prints strings in reverse
+ * @s: The string to be passed in
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+}
