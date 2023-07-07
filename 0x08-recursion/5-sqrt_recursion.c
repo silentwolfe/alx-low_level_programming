4b2300ba5e8a768e430a91acd72ac097b4c08a44
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-07-06 21:44:37 -0700

    Second commits for the sqrt function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,46 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - This function returns the natural square root of a number
- * @n: The number to find the square root of
- * Return: The natural square root of n, or -1 if n does not have a natural square root
+ * sqrt_recursive_helper - The sqrt_recursive_helper function performs a
+ * binary search by
+ * calculating the middle value (mid) and
+ * checking if its square is equal to n.
+ * @n: Find the square root of n recursively
+ * @start: starting time
+ * @end: End time
+ * Return: integer
+ */
+
+int sqrt_recursive_helper(int n, int start, int end)
+{
+	int mid;
+
+	if (start > end)
+	{
+		return (-1);
+	}
+
+	mid = start + (end - start) / 2;
+
+	if (mid * mid == n)
+	{
+		return (mid);
+	}
+	else if (mid * mid < n)
+	{
+		return (sqrt_recursive_helper(n, mid + 1, end));
+	}
+	else
+	{
+		return (sqrt_recursive_helper(n, start, mid - 1));
+	}
+}
+
+/**
+ * _sqrt_recursion -  A function that returns the natural
+ * square root of a number.
+ * @n: input from user
+ * Return: integer
  */
 
 int _sqrt_recursion(int n)
@@ -12,26 +49,12 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-
-	if (n == 0)
+	else if (n == 0 || n == 1)
 	{
-		return (0);
-	}
-
-	int mid;
-
-	mid = n / 2;
-
-	if (mid * mid == n)
-	{
-		return (mid);
-	}
-	else if (mid * mid < n)
-	{
-		return (_sqrt_recursion(mid + 1));
+		return (n);
 	}
 	else
 	{
-		return (-1);
+		return (sqrt_recursive_helper(n, 1, n));
 	}
 }
