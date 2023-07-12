cc634ffb6dd31c39d8ad6ff66a04b6c63b8ffd7c
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-07-12 16:39:32 -0700

    Commit for the freegrid function

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - This function frees the memory allocation of a variable
+ * @grid: The address of the 2-dimensional grid
+ * @height: Height of the grid
+ * Return: none
+ */
+
+void free_grid(int **grid, int height)
+{
+	if (grid == NULL)
+	{
+		return;
+	}
+
+	for (int i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
