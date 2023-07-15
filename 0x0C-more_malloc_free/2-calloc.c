ca85234ef80de735a544dfe481ada347bfa29639
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-07-15 08:23:47 -0700

    Initial commit for the calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,12 +14,14 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *ptr;
+
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
-	void *ptr = malloc(nmemb * size);
+	ptr = malloc(nmemb * size);
 	
 	if (ptr != NULL)
 	{
