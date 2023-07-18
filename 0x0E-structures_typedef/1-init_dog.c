3f2ffbcd3ffc862841f44dc2cda955dec64debd2
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-07-17 21:09:31 -0700

    Initial commit for the typedef function

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - The initialized variable
+ * @d: The argument parameters
+ * @name:The argument parameter
+ * @age: The argument parameter
+ * @owner: The argument parameter
+ *
+ * Return: None
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    d->name = name;
+    d->age = age;
+    d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,5 @@ struct dog
 	float age;
 	char *owner;
 };
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
