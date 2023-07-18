22bad3734602403242e2c529d32d672e7cba7b4b
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-07-18 15:00:26 -0700

    Initial commit for the typedef function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,28 +15,28 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-        dog_t *new_dog;
+	dog_t *new_dog;
 
-        new_dog = malloc(sizeof(dog_t));
-        if (new_dog == NULL)
-        {
-                return (NULL);
-        }
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
 
-        new_dog->name = malloc(strlen(name) + 1);
-        new_dog->owner = malloc(strlen(owner) + 1);
+	new_dog->name = malloc(strlen(name) + 1);
+	new_dog->owner = malloc(strlen(owner) + 1);
 
-        if (new_dog->name == NULL || new_dog->owner == NULL)
-        {
-                free(new_dog->name);
-                free(new_dog->owner);
-                free(new_dog);
-                return (NULL);
-        }
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
 
-        strcpy(new_dog->name, name);
-        new_dog->age = age;
-        strcpy(new_dog->owner, owner);
+	strcpy(new_dog->name, name);
+	new_dog->age = age;
+	strcpy(new_dog->owner, owner);
 
-        return (new_dog);
+	return (new_dog);
 }
