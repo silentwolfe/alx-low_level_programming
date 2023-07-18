103d83e468938ba9bf484e0d4505e0d5e40fe3f6
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-07-18 06:46:03 -0700

    Modified the Null character

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
 
 /**
  * init_dog - The initialized variable
@@ -11,6 +13,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
