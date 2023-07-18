b55f212d9b0feffe4a12fe1a868bdb1475f58953
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-07-18 05:38:07 -0700

    Modified the fractional point

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 
 	if (d == NULL)
 	{
-		printf("nil");
+		printf(" ");
 	}
 
 	if (d->name == NULL)
@@ -26,7 +26,7 @@ void print_dog(struct dog *d)
 
 	if (d->age == 0)
 	{
-		printf("Age: (nil)\n");
+		printf("Age: 0.000000\n");
 	}
 	else
 	{
