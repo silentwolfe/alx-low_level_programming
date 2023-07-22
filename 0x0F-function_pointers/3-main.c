aa9c97c59301b89011b7a57e563c2e27fb96194a
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-07-21 17:38:23 -0700

    Initial commit for the main.c, op_function, get_op_function and calc.h files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,36 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: integer
+ */
+
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*operation)(int, int);
+	int num1, num2, result;
+	int (*operation)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    operation = get_op_func(argv[2]);
+	operation = get_op_func(argv[2]);
 
-    if (!operation)
-    {
-        printf("Error\n");
-        return (99);
-    }
+	if (!operation)
+	{
+		printf("Error\n");
+		return (99);
+	}
 
-    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        return (100);
-    }
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
 
-    result = operation(num1, num2);
-    printf("%d\n", result);
+	result = operation(num1, num2);
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
