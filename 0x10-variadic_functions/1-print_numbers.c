aaad3f745f75b2b0efef4a2a91f4669ee6fa96ed
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-07-21 19:12:57 -0700

    initial commit for the print_number file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int num;
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+		printf("%d", num);
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	va_end(args);
+
+	printf("\n");
+}
