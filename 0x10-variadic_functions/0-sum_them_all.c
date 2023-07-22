6d41bbbd313dc44d8f237e2e1d53ce1b4a3ed17d
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-07-21 18:44:02 -0700

    Initial commit for the sum_them_all file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,6 +12,7 @@ int sum_them_all(const unsigned int n, ...)
 	int sum;
 	unsigned int i;
 	va_list args;
+
 	va_start(args, n);
 
 	if (n == 0)
@@ -20,7 +21,7 @@ int sum_them_all(const unsigned int n, ...)
 	}
 
 	sum = 0;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
