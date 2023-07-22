5dae1d6f8dc054d3ffd9e54bb49d35645e135bcc
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-07-21 17:33:25 -0700

    Initial commit for the main.c, op_function, get_op_function and calc.h files

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - This function performs addition
+ * @a: The integer argument.
+ * @b: The integer argument
+ * Return: Returns the sum of the variables.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - This function performs subtraction
+ * @a: The integer argument.
+ * @b: The integer argument
+ * Return: Returns the differnces of the variables.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - This function performs multiplication
+ * @a: The integer argument.
+ * @b: The integer argument
+ * Return: Returns the multiples of the variables.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - This function performs division
+ * @a: The integer argument.
+ * @b: The integer argument
+ * Return: Returns the divided variables.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - This function performs mod
+ * @a: The integer argument.
+ * @b: The integer argument
+ * Return: Returns the mod of the variables.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
