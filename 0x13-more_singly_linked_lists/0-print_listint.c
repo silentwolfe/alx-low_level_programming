bd0ab1636909ea38ea9e207d27344cb78a821294
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-08-01 11:49:37 -0700

    Initial commit for the linked_list program

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "list.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * print_listint - This prints all the element of listint
+ * @h: The first node (head node)
+ *
+ * Return: return the pointer to node
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t* current = h;
+
+	size_t nodeCount = 0;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		nodeCount++;
+	}
+
+	return (nodeCount);
+}
