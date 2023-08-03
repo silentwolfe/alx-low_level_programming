5ccba236a4c026fc064921ed2f22e0ff582850ee
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-08-02 20:03:33 -0700

    Initial commit for the free_listint2 function

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,16 +6,21 @@
  * freelist_int - This function adds a node at the end of the element lists
  * @head: The pointer to the first node of the element
  * Return: Void
-*/
+ */
 
 void free_listint2(listint_t **head)
 {
-    listint_t* current;
+	listint_t* current;
 
-    while (*head != NULL)
-    {
-        current = *head;
-        *head = (*head)->next;
-        free(current);
-    }
+	if (*head == NULL)
+	{
+		return;
+	}
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
 }
