5dc6d74f47b2f9584e861355cea3c9ea8db2472a
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-08-04 17:53:34 -0700

    Added a new fucntion

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - This fucntion converts binary to unsigned integers
+ * @b: contains any characters other than '0' and '1'
+ * Return: returns the binaries
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+    if (b == NULL)
+    {
+        return (0);
+    }
+
+    unsigned int result = 0;
+    int i = 0;
+
+    while (b[i] != '\0')
+    {
+        if (b[i] != '0' && b[i] != '1')
+            return (0);
+
+        result = (result << 1) + (b[i] - '0');
+        i++;
+    }
+
+    return (result);
+}
