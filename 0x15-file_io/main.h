62d41f5993f6af4cce5b152e065f01dd9e15f463
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-08-11 11:03:24 -0700

    Initial commit for the cp function

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define BUFSIZE 1024
+#define BUFFER_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
