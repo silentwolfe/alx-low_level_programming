5880b77f00ec0acf6b05692301b66d2d939b1d1c
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2023-08-11 10:58:21 -0700

    Initial commit for the cp function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,63 +7,51 @@
  * Return: 0 as success
  */
 
-int main(int ac, char **av)
-{
-	int src, dest;
-	char buffer[BUFSIZE];
-	ssize_t n;
+int main(int argc, char **argv) {
+    int fd_from, fd_to;
+    ssize_t bytes_read, bytes_written;
+    char buffer[BUFFER_SIZE];
+    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+    if (argc != 3) {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
 
-	src = open(av[1], O_RDONLY);
+    fd_from = open(argv[1], O_RDONLY);
+    if (fd_from == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
 
-	if (src == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+    if (fd_to == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+        exit(99);
+    }
 
-	dest = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
+    while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
+        bytes_written = write(fd_to, buffer, bytes_read);
+        if (bytes_written != bytes_read || bytes_written == -1) {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            exit(99);
+        }
+    }
 
-	if (dest == -1)
-	{
-		dprintf(2, "Error: Can't write to file %s\n", av[2]);
-		exit(99);
-	}
+    if (bytes_read == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
 
-	while ((n = read(src, buffer, sizeof(buffer))) > 0)
-	{
-		if (write(dest, buffer, n) != n)
-		{
-			perror("Write error");
-			close(src);
-			close(dest);
-			return (1);
-		}
-	}
+    if (close(fd_from) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+        exit(100);
+    }
 
-	if (n == -1)
-	{
-		perror("Read error");
-		close(src);
-		close(dest);
-		return (1);
-	}
+    if (close(fd_to) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+        exit(100);
+    }
 
-	close(src);
-	close(dest);
-
-	if (chmod(av[2], 0664) == -1)
-	{
-		perror("Chmod error");
-		return (-1);
-	}
-
-	printf("File transfer successful\n");
-	return (0);
+    return 0;
 }
-
