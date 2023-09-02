606272e5e2dae5b7bf76b31d21a9ed506b9deb31
Eboizegbe Osaro <eboizegbeosaro1@gmail.com>
2023-09-02 08:38:42 -0700

    Creation of the set_bit function

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,11 +5,13 @@
  * set_bit - This function sets the value of a bit to 1 at a given index
  * @n: The number passed in by the user
  * @index: The index of
+ * Return: returns 1 if it works or -1 if it fails
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
