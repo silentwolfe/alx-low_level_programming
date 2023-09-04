a11408d077055b5def5cf309785d0690a5ad2057
Eboizegbe Osaro <eboizegbeosaro1@gmail.com>
2023-09-04 15:11:41 -0700

    Created the cp function

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,78 @@
+#include "main.h"
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - This function has the property of copying the
+ * content of one file to another file
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: returns 0 at the end of the program
+ */
+int main(int argc, char *argv[])
+{
+	const char *file_from, *file_to;
+	int fd_from, fd_to;
+	ssize_t nread, nwritten;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+	file_from = argv[1];
+	file_to = argv[2];
+
+	fd_from = open(file_from, O_RDONLY);
+
+	if (fd_from == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd_to == -1)
+	{
+		fprintf(stderr, "Error: Can't write to file %s\n", file_to);
+		exit(99);
+	}
+
+	while ((nread = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		nwritten = write(fd_to, buffer, nread);
+
+		if (nwritten == -1)
+		{
+			fprintf(stderr, "Error: Can't write to file %s\n", file_to);
+			exit(99);
+		}
+		if (nwritten != nread)
+		{
+			fprintf(stderr, "Error: Write operation incomplete\n");
+			exit(99);
+		}
+	}
+
+	if (nread == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		fprintf(stderr, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		fprintf(stderr, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+	return (0);
+}
