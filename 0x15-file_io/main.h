e2220cc677563fb8dff9e8f3cf2080df03463781
Eboizegbe Osaro <eboizegbeosaro1@gmail.com>
2023-09-05 02:58:15 -0700

    Update to the main header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define BUFFSIZE 1024
+#define BUFFSIZE 6000
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
