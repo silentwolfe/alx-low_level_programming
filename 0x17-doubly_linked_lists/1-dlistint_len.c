84fae0ad24ec0c8e51bce2c675f3e440a9e52d02
Eboizegbe osaro <eboizegbeosaro1@gmail.com>
2024-03-21 20:39:23 -0700

    commit successfully

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - This function prints out all the elements of a DLL
+ * dlistint_len - This function prints out all the elements of a DLL
  *
  * @h: Parameter to be passed as argument for the function
  * Return: returns an integer
@@ -10,14 +10,14 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-        size_t node_count = 0;
+	size_t node_count = 0;
 
 
-        while (h != NULL)
-        {
-                h = h->next;
-                node_count++;
-        }
+	while (h != NULL)
+	{
+		h = h->next;
+		node_count++;
+	}
 
-        return (node_count);
+	return (node_count);
 }
